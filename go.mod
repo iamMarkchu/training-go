84435aa0789a5fece4158f44f7fcbf8f513751c8
kelai <kelai@douyu.tv>
2022-03-02 18:40:22 +0800

    项目初始化-swagger集成

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/go-kratos/kratos/v2 v2.2.0
-	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
+	github.com/go-kratos/swagger-api v1.0.1
 	github.com/google/wire v0.5.0
 	google.golang.org/genproto v0.0.0-20220302033224-9aa15565e42a
 	google.golang.org/grpc v1.44.0
--- internal/server/http.go
+++ internal/server/http.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/swagger-api/openapiv2"
 	userApiV1 "trainings-go/api/user/v1"
 	"trainings-go/internal/conf"
 	"trainings-go/internal/service"
@@ -26,6 +27,8 @@ func NewHTTPServer(c *conf.Server, user *service.UserService, logger log.Logger)
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
+	openAPIhandler := openapiv2.NewHandler()
+	srv.HandlePrefix("/q/", openAPIhandler)
 	userApiV1.RegisterUserHTTPServer(srv, user)
 	return srv
 }
